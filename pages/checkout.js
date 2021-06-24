0fc45da8823ffaf966256df6b6d516e7152782ad
Alex Gomes <alex.gduguay@gmail.com>
2021-06-24 23:17:52 +0100

    Add disclaimer

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,4 +1,5 @@
-import { NextSeo } from 'next-seo'
+import { useState } from "react";
+import { NextSeo } from "next-seo";
 import Page from "../components/styled/Page";
 import useCart from "../hooks/useCart";
 import styled from "styled-components";
@@ -34,10 +35,21 @@ const Button = styled.button`
   padding: 1rem;
   color: #fff;
   cursor: pointer;
+  border: 1px solid #fff;
+`;
+
+const ConfirmContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
 `;
 
 const Checkout = () => {
   const { cart, total } = useCart();
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const processPayment = async () => {
     const url = "/.netlify/functions/charge-card";
@@ -45,17 +57,17 @@ const Checkout = () => {
       id,
       qty,
     }));
-    
+
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
     const { data } = await axios.post(url, { newCart });
     console.log(data);
-    console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-    await stripe.redirectToCheckout({ sessionId: data.id })
+    console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    await stripe.redirectToCheckout({ sessionId: data.id });
   };
 
   return (
     <Page>
-      <NextSeo title='Checkout' description='SoopaShop checkout page'/>
+      <NextSeo title="Checkout" description="SoopaShop checkout page" />
       <h2>Checkout</h2>
       {cart.length > 0 ? (
         <>
@@ -75,7 +87,21 @@ const Checkout = () => {
             <span>Total</span>
             <span>£{total / 100}</span>
           </Total>
-          <Button onClick={processPayment}>Process Payment</Button>
+          <Button onClick={() => setIsCheckout(true)}>Process Payment</Button>
+          {isCheckout && (
+            <ConfirmContainer>
+              <p>
+                Disclaimer! £5 will be charged for every item unit and you will not
+                receive the item itself! These are just fictional products for
+                the purpose of the app however the money will be a donation and
+                much appreciated! Thank you :).
+              </p>
+              <ButtonContainer>
+                <Button onClick={processPayment}>Confirm</Button>
+                <Button onClick={() => setIsCheckout(false)}>Cancel</Button>
+              </ButtonContainer>
+            </ConfirmContainer>
+          )}
         </>
       ) : (
         <p>You do not appear to have any items in your cart!</p>
